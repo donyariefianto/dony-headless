269f35c6450c1018a91f6254d29834574d6fde2c
donyariefianto <56543110+donyariefianto@users.noreply.github.com>
2025-08-12 00:04:49 +0700

    Update dataEntries.js

--- resources/UI/js/pages/dataEntries.js
+++ resources/UI/js/pages/dataEntries.js
@@ -228,16 +228,51 @@ export const setupDataEntries = () => {
       let hasMore = true
       let currentSearchTerm = ''
       if (element.classList.contains('choices-select--initialized')) {
-        console.log('Choices.js sudah diinisialisasi pada elemen ini. Melewati.')
+        // console.log('Choices.js sudah diinisialisasi pada elemen ini. Melewati.')
         return // Skip ke elemen berikutnya
       }
       element.classList.add('choices-select--initialized')
       const choices = new Choices(element, {
         searchEnabled: true,
         searchChoices: false,
-        itemSelectText: 'Pilih',
         noResultsText: 'Tidak ada hasil yang ditemukan', // Pesan jika tidak ada hasil pencarian
         noChoicesText: 'Pilihan tidak di temukan', // Pesan ketika tidak ada pilihan sama sekali
+
+        loadingText: 'Loading...',
+        itemSelectText: 'Press to select',
+        uniqueItemText: 'Only unique values can be added',
+        customAddItemText: 'Only values matching specific conditions can be added',
+        classNames: {
+          containerOuter: ['choices'],
+          containerInner: ['choices__inner'],
+          input: ['choices__input'],
+          inputCloned: ['choices__input--cloned'],
+          list: ['choices__list'],
+          listItems: ['choices__list--multiple'],
+          listSingle: ['choices__list--single'],
+          listDropdown: ['choices__list--dropdown'],
+          item: ['choices__item'],
+          itemSelectable: ['choices__item--selectable'],
+          itemDisabled: ['choices__item--disabled'],
+          itemChoice: ['choices__item--choice'],
+          description: ['choices__description'],
+          placeholder: ['choices__placeholder'],
+          group: ['choices__group'],
+          groupHeading: ['choices__heading'],
+          button: ['choices__button'],
+          activeState: ['is-active'],
+          focusState: ['is-focused'],
+          openState: ['is-open'],
+          disabledState: ['is-disabled'],
+          highlightedState: ['is-highlighted'],
+          selectedState: ['is-selected'],
+          flippedState: ['is-flipped'],
+          loadingState: ['is-loading'],
+          notice: ['choices__notice'],
+          addChoice: ['choices__item--selectable', 'add-choice'],
+          noResults: ['has-no-results'],
+          noChoices: ['has-no-choices'],
+        },
       })
 
       const fetchData = async (page, searchTerm = '', endpoint, value, label) => {
